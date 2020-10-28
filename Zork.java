73583fd9ef8d2ecbd2b754e394204b7de82dacc9
omughar <omughar@csu.fullerton.edu>
2020-10-28 00:46:51 -0700

    secondary

--- Zork.java
+++ Zork.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-enum Direction{NORTH, SOUTH, WEST, EAST;}
+enum Direction{NORTH, SOUTH, WEST, EAST};
+enum ColItemType{HEALING, WEAPON, LIGHT, TREASURE};
 
 class ZorkObj{
     public String status;
@@ -14,15 +15,27 @@ abstract class Conditional{
 
 public class PlayerHas extends Conditional{
     public String playerName;
-    public boolean confirm(Player){}
-    public PlayerHas(){this("");}
+    public boolean confirm(Player){return true;}
+    public PlayerHas(){}
 }
 
-public class Room extends ZorkObj{
+class Room extends ZorkObj{
     private String name;
     private String desc;
     private Map<Direction,Room> borders;
     public Room(){this.name=""; this.desc="";}
+}
+
+class ColItem extends ZorkObj{
+    private String name;
+    private ColItemType type;
+    public ColItem(){}
+}
+
+class Item extends ZorkObj{
+    private String name;
+    private ItemType type;
+    public Item(){}
 }
 
 class Player{}
