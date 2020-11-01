dcd2ac37a1593c32e459177367888b54443f93d7
omughar <omughar@csu.fullerton.edu>
2020-11-01 00:13:19 -0700

    developed maze class

--- Zork.java
+++ Zork.java
@@ -37,23 +37,51 @@ public class PlayerHas extends Conditional{
     public PlayerHas(){}
 }
 
-class Room extends ZorkObj{
-    private String name;
-    private String desc;
-    private Room northN;
+class Map extends ZorkObj{
+    /*private String name;
+    private String desc;*/
+    public static final int NORTH=1;
+    public static final int EAST=2;
+    public static final int SOUTH=3;
+    public static final int WEST=4;
+    public static final int NO_DIRECTION=0;
+    private static String[] walls={"North","East","South","West"};
+    private Room[][] rooms;
+    /*private Room northN;
     private Room southN;
     private Room westN;
     private Room eastN;
-    //private Map<Direction,Room> borders;
-    public Room(){this.name=""; this.desc="";}
-    public Room getNorthN(){return northN;}
+    //private Map<Direction,Room> borders;*/
+
+    /*public Room getNorthN(){return northN;}
     public void setNorthN(Room room){northN=room;}
     public Room getSouthN(){return southN;}
     public void setSouthN(Room room){southN=room;}
     public Room getWestN(){return westN;}
     public void setWestN(Room room){westN=room;}
     public Room getEastN(){return eastN;}
-    public void setEastN(Room room){eastN=room;}
+    public void setEastN(Room room){eastN=room;}*/
+    public boolean isClosed(int row,int column,int wall){
+        return rooms[row][column].wallList[wall];
+    }
+
+    public Map(){}
+
+
+}
+
+class Room{
+    private String name;
+    private String desc;
+    boolean[] wallList;
+    Room(String name,String desc){
+        this.name=name;
+        this.desc=desc;
+        wallList=new boolean[4];
+        for(int i=0;i<4;i++){
+            wallList[i]=true;
+        }
+    }
 }
 
 class ColItem extends ZorkObj{
