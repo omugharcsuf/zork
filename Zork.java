ca455c7073f4f5030230f3c62334d2dab8c6a59b
omughar <omughar@csu.fullerton.edu>
2020-11-01 14:34:56 -0800

    added exit room method

--- Zork.java
+++ Zork.java
@@ -1,17 +1,17 @@
 //enum Direction{NORTH, SOUTH, WEST, EAST};
 enum ColItemType{HEALING, WEAPON, LIGHT, TREASURE};
-enum ItemType{SWITCH, BOX}my
+enum ItemType{SWITCH, BOX};
 
 class Zork{
     private int hp;
-    private Room currentRoom;
+    public static int currentRow;
+    public static int currentColumn;
     public int atk;
     public int def;
     public ColItem[] inventory=new ColItem[10];
 
-    Zork(Room currentRoom){
+    Zork(){
         this.hp=20;
-        this.currentRoom=currentRoom;
         this.atk=10;
         this.def=10;
     }
@@ -28,23 +28,23 @@ class ZorkObj{
 
 abstract class Conditional{
     public String in;
-    public abstract boolean confirm(Player);
+    public abstract boolean confirm(Zork player);
 }
 
-public class PlayerHas extends Conditional{
+class PlayerHas extends Conditional{
     public String playerName;
-    public boolean confirm(Player){return true;}
+    public boolean confirm(Zork player){return true;}
     public PlayerHas(){}
 }
 
 class Map extends ZorkObj{
     /*private String name;
     private String desc;*/
-    public static final int NORTH=1;
-    public static final int EAST=2;
-    public static final int SOUTH=3;
-    public static final int WEST=4;
-    public static final int NO_DIRECTION=0;
+    public static final int NORTH=0;
+    public static final int EAST=1;
+    public static final int SOUTH=2;
+    public static final int WEST=3;
+    public static final int NO_DIRECTION=4;
     private static String[] walls={"North","East","South","West"};
     private Room[][] rooms;
     /*private Room northN;
@@ -61,11 +61,51 @@ class Map extends ZorkObj{
     public void setWestN(Room room){westN=room;}
     public Room getEastN(){return eastN;}
     public void setEastN(Room room){eastN=room;}*/
-    public boolean isClosed(int row,int column,int wall){
+    public boolean isWall(int row,int column,int wall){
         return rooms[row][column].wallList[wall];
     }
 
-    public Map(){}
+    public void setWall(int row,int column,int wall){
+        rooms[row][column].wallList[wall]=true;
+    }
+
+    public void setDoor(int row,int column,int wall){
+        rooms[row][column].wallList[wall]=false;
+    }
+
+    public boolean exitRoom(int wall){
+        if(rooms[Zork.currentRow][Zork.currentColumn].wallList[wall]=true)return false;
+        else{
+            if(wall==0){
+                Zork.currentRow++;
+                return true;
+            }
+            else if(wall==1){
+                Zork.currentColumn++;
+                return true;
+            }
+            else if(wall==2){
+                Zork.currentRow--;
+                return true;
+            }
+            else if(wall==3){
+                Zork.currentColumn--;
+                return true;
+            }
+            else return false;
+        }
+    }
+
+    public Map(int rows,int columns){
+        String name;
+        String desc;
+        rooms=new Room[rows][columns];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<rows;j++){
+                rooms[i][j]=new Room(name,desc);
+            }
+        }
+    }
 
 
 }
